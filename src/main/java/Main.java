938d48605d6078156a8da3e0cee340d849b308a6
Manpreet Singh <raj2774@gmail.com>
2020-11-25 20:05:18 -0500

    Second Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a new Project - Second Pipeline");
+        System.out.println("Some Changes Made");
     }
 }
